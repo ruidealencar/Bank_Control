f5cbe6dd15905129435cac06af1397978edc0b71
Rui <ruidealencar@gmail.com>
2018-05-28 20:31:59 -0900

    Formatando valores monetários

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -72,4 +72,5 @@ class ReleasesController < ApplicationController
       params.require(:release).permit(:account_id, :release_type, :value, :account_number)
       
     end
+    
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+    def valor_formatado(number)
+        number_to_currency number,
+          unit: "R$",
+          separator: ",",
+          delimiter: "."
+      end
 end
+
+#método para sacar e creditar no saldo.
+def saque(value)
+    # account = Account.find(params[:id]).
+    # account = Account.find(self.account_id)
+    if value <= 0 
+      return false
+    else
+        account.balance += value
+    end
+  end
+
+  #método para depositar e debitar no saldo.  
+  def deposito(value)
+    if account.balance >= value
+      account.balance -= value
+    else
+      return false
+    end  
+  end
